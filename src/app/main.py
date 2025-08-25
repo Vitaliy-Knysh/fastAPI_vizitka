041783183459d95b9e3f55fff12bb437e1f83f81
Vitaliy-Knysh <mr.knyshjr2@gmail.com>
2025-08-26 02:43:40 +0300

    .

--- src/app/main.py
+++ src/app/main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
 def get_root(request: Request):
-    response_kwargs = {}
-    return templates.TemplateResponse("index.html", response_kwargs)
+    context = {
+        "title": "Мой замечательный сайт-визитка",
+        "some_content": "КОНТЕНТ",
+        "request": request
+    }
+    return templates.TemplateResponse("index.html", context)
